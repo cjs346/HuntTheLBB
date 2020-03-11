1293924cfdaab2c40bdbeac7c14e15e044ad1524
wangzai1103 <31539583+wangzai1103@users.noreply.github.com>
2020-03-11 14:06:45 +0000

    add cavern

--- HuntTheLBB/HuntTheWumpus/src/Cavern.java
+++ HuntTheLBB/HuntTheWumpus/src/Cavern.java
@@ -1,4 +1,34 @@
 public class Cavern {
+	private int id = -1;
+	private boolean hasPits;
+	private boolean hasBats;
+	
+	public Cavern(int id) {
+		this.id = id;
+	}
+		
+	public int getId() {
+		return id;
+	}
+	
+
+	public boolean HasPits() {
+		return hasPits;
+	}
+	
+	public void setHasPits(boolean hasPits) {
+		this.hasPits = hasPits;
+	}
+	
+	public boolean HasBats() {
+		return hasBats;
+	}
+	
+	public void setHasBats(boolean hasBats) {
+		this.hasBats = hasBats;
+	}
+	
+	
 
 
 }
