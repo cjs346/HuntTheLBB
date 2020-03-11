d1684b104e4d043bcaefc0b366bc85839b09d4fe
cjs346 <cjs346@cornell.edu>
2020-03-11 10:58:12 -0400

    Update HuntTheWumpus.java

--- HuntTheLBB/HuntTheWumpus/src/HuntTheWumpus.java
+++ HuntTheLBB/HuntTheWumpus/src/HuntTheWumpus.java
@@ -4,28 +4,31 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HuntTheWumpus {
 
     @Test
     public void canMoveCavernToCavern() throws Exception{
-        Map map = new Map();
+        Map map = new MapGenerator().generateStaticMap();
 
-        map.connect(1,2, Direction.EAST);
 
         Game game = new Game(map, new MessengerSpy());
-        game.insertPlayer(1);
-        game.move(Direction.EAST);
+        game.insertPlayer(0);
 
+        game.move(Direction.SOUTH);
+
+        assertEquals(1, game.getPlayerLocation());
+        game.move(Direction.EAST); 
+        
         assertEquals(2, game.getPlayerLocation());
-        map.connect(2,3, Direction.NORTH);
-        game.move(Direction.NORTH);
-        assertEquals(3, game.getPlayerLocation());
     }
 
     @Test
     public void testArrowMessage() {
-        Map map = new Map();
+        Map map = new Map(0);
         Game game = new Game(map, new MessengerSpy());
 
         assertEquals("[You have 5 arrows left. /ln]" ,game.EndTurn());
@@ -35,13 +38,12 @@ public class HuntTheWumpus {
     
     @Test
     public void testPlayerDeath() {
-        Map map = new Map();
-        map.connect(1,-1, Direction.EAST);
-        
+    		
+    	Map map = new MapGenerator().generateStaticMap();
+
         Game game = new Game(map, new MessengerSpy());
         
-        game.insertPlayer(1);
-        game.move(Direction.EAST);
+        game.move(Direction.REST);
         
         assertEquals("[wumpus murdered you /ln]" , game.EndTurn());
         assertFalse(game.isGameOn);	
